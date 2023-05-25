91989d80138439cba43f75026322e86c9595d758
THANUSAN23 <134580784+THANUSAN23@users.noreply.github.com>
2023-05-25 11:36:23 +0530

    Add files via upload

---
+++ thanusan-age.py
@@ -0,0 +1,5 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:34:50) [MSC v.1934 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+12
